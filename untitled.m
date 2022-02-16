73053a2ae2288394dc465ead1c216d257a997a3a
Evan Kemmer <evankemmer@gmail.com>
2022-02-16 13:49:13 +0100

    updated matlab script

--- untitled.m
+++ untitled.m
@@ -8,13 +8,6 @@ for iFile = 1:numel(FileList)               % Loop over found files
     if isfield(allData, aField)             % Attach new data:
        allData.(aField) = [allData.(aField), Data.(aField)];
        
-       % [EDITED]
-       % The orientation depends on the sizes of the fields. There is no
-       % general method here, so maybe it is needed to concatenate 
-       % vertically:
-       % allData.(aField) = [allData.(aField); Data.(aField)];
-       % Or in general with suiting value for [dim]:
-       % allData.(aField) = cat(dim, allData.(aField), Data.(aField));
     else
        allData.(aField) = Data.(aField);
     end
